88e8dff9741c6dc04c847673e606c08a1f7a3239
Otávio Augusto Alves <47834738+otavioaugusto813@users.noreply.github.com>
2024-03-26 18:03:37 -0300

    Mudando o horário de início.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 26, 2024 16:28:00").getTime();
+var countDownDate = new Date("Aug 27, 2024 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
